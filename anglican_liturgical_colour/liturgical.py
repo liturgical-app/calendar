f837b67c7ca4907e34bc89f6a2cf0e0e7bfda53a
Jonathan Gazeley <me@jonathangazeley.com>
2023-12-10 20:41:12 +0000

    Reimplement Date_to_Days from Perl's Date::Calm

--- anglican_liturgical_colour/liturgical.py
+++ anglican_liturgical_colour/liturgical.py
@@ -247,6 +247,21 @@ sub advent_sunday {
     return -(Day_of_Week($y,12,25) + 4*7);
 }
 
+def Date_to_Days(year, month, day):
+
+    # Define a start date as passed in
+    f_date = date(year, month, day)
+
+    # Define an end date as 1st of January of the year 1 A.D.
+    # This is defined in Perl Date::Calm
+    l_date = date(1, 1, 1)
+
+    # Calculate the difference between the end date and start date
+    delta = l_date - f_date
+
+    # Print the number of days in the time difference
+    return(delta.days)
+
 ##########################################################################
 
 sub new {
