3bcab450b12c4acd5d4044282234587069f8fdd9
Jonathan Gazeley <me@jonathangazeley.com>
2024-03-01 17:02:47 +0000

    Improve docstrings in funcs

--- liturgical_colour/funcs.py
+++ liturgical_colour/funcs.py
@@ -8,6 +8,17 @@ from dateutil.easter import easter
 def get_easter(year: int) -> tuple:
     """
     Returns the date easter occurs on for a given year as (month,day)
+
+    Parameters
+    ----------
+      year: int
+        The year to perform the calculation for
+    Return
+    ------
+      month: int
+        The month that Easter falls in
+      day: int
+        The day that Easter falls on
     """
     easter_date = easter(year)
     easter_month = easter_date.month
@@ -18,12 +29,35 @@ def get_easter(year: int) -> tuple:
 def get_advent_sunday(year: int) -> int:
     """
     Return date of Advent Sunday, in days relative to Christmas day
+    This is always negative as Advent Sunday is before Christmas day
+
+    Parameters
+    ----------
+      year: int
+        The year to perform the calculation for
+    Return
+    ------
+      offset: int
+        The numnber of days that Advent Sunday falls before Christmas day
     """
     return -(day_of_week(year,12,25) + 3*7)
 
 def date_to_days(year: int, month: int, day: int) -> int:
     """
     Convert a date from year,month,day to days since 1st January, 1 AD
+
+    Parameters
+    ----------
+      year: int
+        The year to convert
+      month: int
+        The month to convert
+      day: int
+        The day to convert
+    Return
+    ------
+      offset: int
+        The numnber of days since 1st January, 1 AD
     """
     # Define a start date as passed in
     f_date = date(year, month, day)
@@ -38,6 +72,19 @@ def day_of_week(year: int, month: int, day: int) -> int:
     Compare with:
       weekday() which is 0-6 and 0 is Monday
       isoweekday() which is 1-7 and 1 is Monday
+
+    Parameters
+    ----------
+      year: int
+        Year to perform the calculation for
+      month: int
+        Month to perform the calculation for
+      day: int
+        Day to perform the calculation for
+    Return
+    ------
+      weekday: int
+        Numerical day of the week in the range 0-6, where 0 is Sunday
     """
 
     # Define a start date as passed in
@@ -52,6 +99,19 @@ def day_of_week(year: int, month: int, day: int) -> int:
 def add_delta_days(days: int) -> tuple:
     """
     Convert days since 1st January, 1 AD back to year,month, day
+
+    Parameters
+    ----------
+      days: int
+        Days since 1st January, 1 AD
+    Return
+    ------
+      year: int
+        Year of the date [days] since 1st January, 1 AD
+      month: int
+        Month of the date [days] since 1st January, 1 AD
+      day: int
+        Day of the date [days] since 1st January, 1 AD
     """
     epoch = date(1, 1, 1)
     end_date = epoch + timedelta(days=days)
@@ -60,7 +120,16 @@ def add_delta_days(days: int) -> tuple:
 
 def colour_code(colour: str) -> str:
     """
-    Accept a colour name and return a colour code
+    Accept a colour name and return a hex colour code
+
+    Parameters
+    ----------
+      colour: str
+        The name of a colour
+    Return
+    ------
+      code: str
+        The corresponding hex code for the named colour
     """
     codes = {
         'white': '#ffffff',
